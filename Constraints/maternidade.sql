ac5514b4d4a078806e504a5baa0d54345462e01c
Lucas Sartorelli <lucaspmsartorelli@gmail.com>
2026-05-01 00:03:48 -0300

    feature: finished

--- Constraints/maternidade.sql
+++ Constraints/maternidade.sql
@@ -7,12 +7,17 @@ USE maternidade
 GO
 
 CREATE  TABlE   mae(
-    id                      INT             NOT NULL,
+    id                      INT             NOT NULL
+        IDENTITY(1001,1),
     nome                    VARCHAR(60)     NOT NULL,
     logradouro_endereco     VARCHAR(100)    NOT NULL,
-    cep_endereco            CHAR(8)         NOT NULL,
+    num_endereco            INT             NOT NULL
+        CHECK(num_endereco >=0 ),
+    cep_endereco            CHAR(8)         NOT NULL
+        CHECK(LEN(cep_endereco) = 8),
     complemento_endereco    CHAR(200)       NOT NULL,
-    telefone                CHAR(10)        NOT NULL,
+    telefone                CHAR(10)        NOT NULL
+        CHECK(LEN(telefone) = 10),
     data_nascimento         DATE            NOT NULL,
 
     PRIMARY KEY (id)
@@ -22,7 +27,8 @@ CREATE  TABLE   medico(
     crm_numero              INT             NOT NULL,
     crm_uf                  CHAR(2)         NOT NULL,
     nome                    VARCHAR(60)     NOT NULL,
-    telefone_celular        CHAR(11)        NOT NULL,
+    telefone_celular        CHAR(11)        NOT NULL
+        UNIQUE  CHECK(LEN(telefone_celular) = 11),
     especialidade           VARCHAR(30)     NOT NULL,
 
     PRIMARY KEY (crm_numero,crm_uf)
@@ -31,11 +37,15 @@ CREATE  TABLE   medico(
 GO
 
 CREATE  TABLE   bebe(
-    id                      INT             NOT NULL,
+    id                      INT             NOT NULL
+        IDENTITY(1,1),
     nome                    VARCHAR(60)     NOT NULL,
-    data_nasc               DATE            NOT NULL,
-    altura                  DECIMAL(7,2)    NOT NULL,
-    peso                    DECIMAL(4,3)    NOT NULL,
+    data_nasc               DATE            NOT NULL
+        DEFAULT GETDATE(),
+    altura                  DECIMAL(7,2)    NOT NULL
+        CHECK(altura >= 0),
+    peso                    DECIMAL(4,3)    NOT NULL
+        CHECK(peso >= 0),
     id_mae                  INT             NOT NULL,
 
     PRIMARY KEY (id),
